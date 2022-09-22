eed1753561a8e4d8105891c779ff31d4d6b1861c
bmcnamee96 <bmac965@gmail.com>
2022-09-22 14:21:59 -0400

    looked at meat impact

--- sql.sql
+++ sql.sql
@@ -1,11 +1,12 @@
-CREATE TABLE food_name
+-- Create the tables food_name and food_data
+CREATE TABLE IF NOT EXISTS food_name
 (id int primary key,
  name varchar(35),
  year int
 );
 -- DROP TABLE food_name;
 
-CREATE TABLE food_data
+CREATE TABLE IF NOT EXISTS food_data
 (id int,
  emissions_kilogram NUMERIC,
  emissions_1000_kilocalories NUMERIC,
@@ -32,15 +33,88 @@ CREATE TABLE food_data
  biodiversity_100gprotein NUMERIC,
  biodiversity_100gfat NUMERIC
 );
+-- DROP TABLE food_data;
 
+-- After tables are created, add the data
+-- Data received from 'Our World in Data'
+
+-- check the tables to ensure that all the data has been imported
 SELECT * FROM food_name;
 SELECT * FROM food_data;
 
+-- run basic SQL queries
+-- Find the total emissions for all land animals 
 SELECT 
-food_name.name,
-food_data.emissions_kilogram
-FROM food_name
-JOIN food_data
-ON food_name.id = food_data.id
-WHERE food_data.emissions_kilogram > 15
-ORDER BY food_data.emissions_kilogram;
+fn.name,
+fd.emissions_kilogram
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Chicken%' OR fn.name LIKE 'Lamb%' OR fn.name LIKE 'Goat%' OR fn.name LIKE 'Pork%' OR fn.name LIKE 'Beef%'
+ORDER BY fd.emissions_kilogram;
+-- SUM the total emissions
+SELECT
+COUNT(fn.name),
+SUM(fd.emissions_kilogram)
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Chicken%' OR fn.name LIKE 'Lamb%' OR fn.name LIKE 'Goat%' OR fn.name LIKE 'Pork%' OR fn.name LIKE 'Beef%';
+-- SUM the chicken emissions
+SELECT
+COUNT(fn.name),
+SUM(fd.emissions_kilogram)
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Chicken%';
+-- SUM the lamb emissions
+SELECT
+COUNT(fn.name),
+SUM(fd.emissions_kilogram)
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Lamb%';
+-- SUM the goat emissions
+SELECT
+COUNT(fn.name),
+SUM(fd.emissions_kilogram)
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Goat%';
+-- SUM the pork emissions
+SELECT
+COUNT(fn.name),
+SUM(fd.emissions_kilogram)
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Pork%';
+-- SUM the beef emissions
+SELECT
+COUNT(fn.name),
+SUM(fd.emissions_kilogram)
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Beef%';
+
+-- Find the total emissions for all sea animals
+SELECT 
+fn.name,
+fd.emissions_kilogram
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Cod%' OR fn.name LIKE 'Haddock%' OR fn.name LIKE 'Mackerel%' OR fn.name LIKE 'Prawn%' OR fn.name LIKE 'Salmon%' OR fn.name LIKE 'Tuna%'
+ORDER BY fd.emissions_kilogram;
+-- SUM the total emissions
+SELECT
+COUNT(fn.name),
+SUM(fd.emissions_kilogram)
+FROM food_name as fn
+JOIN food_data as fd
+ON fn.id = fd.id
+WHERE fn.name LIKE 'Cod%' OR fn.name LIKE 'Haddock%' OR fn.name LIKE 'Mackerel%' OR fn.name LIKE 'Prawn%' OR fn.name LIKE 'Salmon%' OR fn.name LIKE 'Tuna%';
